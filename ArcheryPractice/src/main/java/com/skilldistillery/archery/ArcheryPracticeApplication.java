a877e7bdeaca940cedb8c28a47756f5affc4e9f0
Reid Snyder <reidsnyder7@gmail.com>
2023-09-15 17:35:14 -0600

    create boot mvc, connect jpa project dependency

---
+++ ArcheryPractice/src/main/java/com/skilldistillery/archery/ArcheryPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.archery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArcheryPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArcheryPracticeApplication.class, args);
+	}
+
+}
---
+++ ArcheryPractice/src/test/java/com/skilldistillery/archery/ArcheryPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.archery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArcheryPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
